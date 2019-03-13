4ae301677cf044b43ce3cde7be5090fce72d9230
Nick Solomon <nsolomon@council.nyc.gov>
2019-03-13 10:53:45 -0400

    Revert "rebuild fix"
    This reverts commit 43487b402be593313512cab1c001f919580679f9.

--- script.R
+++ script.R
@@ -1294,7 +1294,6 @@ if(is.data.frame(e) && nrow(e)!=0) { ## saving a copy of original
 
   write.csv(eres_map,'eres_map.csv',row.names=FALSE)
   #eres_map=read.csv('eres_map.csv', stringsAsFactor=FALSE)
-  
 
   #####################################
   # Evictions Residential layer
